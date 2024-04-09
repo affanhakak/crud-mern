4566fd4144bdf8610cc2d89598a611b50d18f55f
Affan Hakak <affanshowkat@gmail.com>
2024-04-09 21:16:30 +0530

    added delete operations

--- server/index.js
+++ server/index.js
@@ -34,6 +34,13 @@ app.put("/updateuser/:id", (req, res) => {
     .catch((err) => res.json(err));
 });
 
+app.delete("/deleteUser/:id", (req, res) => {
+  const id = req.params.id;
+  userModel.findByIdAndDelete(
+    { _id: id }.then((res) => res.json(res)).catch((err) => res.json(err))
+  );
+});
+
 app.post("/createUser", (req, res) => {
   UserModel.create(req.body).then((users) =>
     res.json(users).catch((err) => res.json(err))
